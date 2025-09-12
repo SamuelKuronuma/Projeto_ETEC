113444ec56fbf8d1bcad36292ce422bbdb53f0b4
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-09-12 10:38:14 -0300

    Adicionados getters e setters na classeprincipal para a classepessoa

--- src/ClassePrincipal.java
+++ src/ClassePrincipal.java
@@ -9,9 +9,15 @@ public class ClassePrincipal {
 		ClassePessoa rafael = new ClassePessoa();
 		
 		luiz.setNome("Luiz");
+		victor.setNome("Victor");
+		rafael.setIdade(16);
 		
 		System.out.println(victor.getNome());
 		System.out.println(luiz.getNome());
+		System.out.println(rafael.getNome());
+		System.out.println(victor.getIdade());
+		System.out.println(luiz.getIdade());
+		System.out.println(rafael.getIdade());
 		
 
 	}
