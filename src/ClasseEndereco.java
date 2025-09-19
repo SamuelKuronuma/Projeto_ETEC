5b07a3df0606f352a5e5634ff4717269666b2d24
SamuelKuronuma <samuel.kuronuma@etec.sp.gov.br>
2025-09-19 10:45:15 -0300

    Adicionados atributos na classe discentes

--- src/ClasseEndereco.java
+++ src/ClasseEndereco.java
@@ -5,4 +5,30 @@ public class ClasseEndereco {
 	String cidadeAt;
 	String estadoAt;
 	
+	public ClasseEndereco(String ruaPar, String cidadePar, String estadoPar) {
+		this.ruaAt = ruaPar;
+		this.cidadeAt = cidadePar;
+		this.estadoAt = estadoPar;
+		
+	}
+	
+	public void setRua(String ruaPar) {
+		this.ruaAt = ruaPar;
+	}
+	public void setCidade(String cidadePar) {
+		this.cidadeAt = cidadePar;
+	}
+	public void setEstado(String estadoPar) {
+		this.estadoAt = estadoPar;
+	}
+	public String getRua(){
+		return ruaAt;
+	}
+	public String getCidade(){
+		return cidadeAt;
+	}
+	public String getEstado(){
+		return estadoAt;
+	}
+	
 }
